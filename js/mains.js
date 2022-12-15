ab7077e8cca01f8d4adb43b6c897b1bbeee11291
robs-am <roberta.amaro89@gmail.com>
2022-12-15 05:46:48 -0300

    js folder

--- js/mains.js
+++ js/mains.js
@@ -1,1 +1,18 @@
-new Glide('.images', {}).mount();
+var input = document.querySelector('.images')
+
+var glide = new Glide('.images', {
+  autoplay: input.value,
+  hoverpause: false,
+  perView: 2
+})
+
+input.addEventListener('input', function (event) {
+  glide.update({
+    autoplay: (event.target.value != 0) ? event.target.value : false
+  })
+})
+
+glide.mount()
+
+
+//new Glide('.images', {}).mount();

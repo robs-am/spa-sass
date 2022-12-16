32575a7c4e400bd9221e5aa30676ffa7ea9564ef
Roberta Amaro <roberta.amaro89@gmail.com>
2022-12-16 08:08:03 -0300

    delete produto-container

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.carousel__lista'),  {
-        slidestoShow: 3;
-        slidesToScroll: 3;
-        draggable: true;
-        dots: '.dots',
-        
-    });
-});
-
 //new Glide('.images', {}).mount();
